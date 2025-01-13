77db52ac651677febc97e8f098c1ec1191b63f43
Shashwat Singh <imshashwatsingh@gmai.com>
2025-01-13 20:06:57 +0530

    tech removed

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
           </div>
           <About />
           <Experience />
-          <Tech />
+          {/* <Tech /> */}
           <Works />
           <div className="relative z-0">
             <Contact />
