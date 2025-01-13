0b4966cf220231e45d277b123d3f01aeb7bd88c7
Shashwat Singh <imshashwatsingh@gmai.com>
2025-01-13 19:38:07 +0530

    portfolio complete1

--- src/assets/index.js
+++ src/assets/index.js
@@ -26,7 +26,7 @@ import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
-import sql from "./tech/PostgreSQL.png";
+import sql from "./tech/postgresql.png";
 
 import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
