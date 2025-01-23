048bada4fd5478bb49a0251f079e90709f2c0760
Shashwat Singh <imshashwatsingh@gmai.com>
2025-01-23 18:53:23 +0530

    footer added !

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,6 +5,7 @@ import { navLinks, profiles } from "../constants";
 import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
+  
   const [active, setActive] = useState("");
 
   const [toggle, setToggle] = useState(false);
